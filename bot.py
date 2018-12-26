07c8e472d8b737dd069d78ff524a09b32dcbff02
faisalburhanudin <faisalburhanudin@.gmail.com>
2018-12-26 12:15:58 +0700

    classifier

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import os
+from sklearn.externals import joblib
 import logging
 
 from telegram.ext import Updater, MessageHandler, Filters
@@ -7,10 +9,21 @@ logging.basicConfig(level=logging.DEBUG,
 
 updater = Updater(token='756768931:AAHk6rE0E7Es7j73lPHnUn51oSJKW9KrfQ0')
 
+current_dir = os.path.dirname(__file__)
+
+tfidf = joblib.load(os.path.join(current_dir, "tfidf.pkl"))
+clf = joblib.load(os.path.join(current_dir, "svm.pkl"))
+
+
+def is_spam(text):
+    transform = tfidf.transform((text,))
+    predict = clf.predict(transform)
+    return bool(predict[0] == 1)
+
 
 def spam_handler(bot, update):
     message = update.message
-    if 'spam' in message.text:
+    if is_spam(message.text):
         bot.delete_message(chat_id=message.chat_id, message_id=message.message_id)
         bot.send_message(chat_id=update.message.chat_id, text="Message deleted, consider as spam")
 
